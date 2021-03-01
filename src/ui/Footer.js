e827c40de7c1a0415b176bf22628182f36e31f1b
ffadilaputra <i.fadilaputra@gmail.com>
2021-03-02 00:24:05 +0700

    first blood

---
+++ src/ui/Footer.js
@@ -0,0 +1,18 @@
+import { Box, Text } from "theme-ui"
+
+const Footer = (props) => (
+  <Box
+    sx={{
+      p: 4,
+      color: "#666",
+      textAlign: "center",
+      fontSize: 1,
+    }}
+  >
+    <Text sx={{ mx: 3, display: "inline-block" }}>
+      Footer
+    </Text>
+  </Box>
+)
+
+export default Footer
